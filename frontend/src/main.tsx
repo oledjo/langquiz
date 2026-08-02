495470c975f823edce8e5fde31488ae9e8909c59
oledjo <oledjo@gmail.com>
2026-08-03 01:27:03 +0200

    feat: wrap app in BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.tsx'
 import { AppErrorBoundary } from './components/AppErrorBoundary.tsx'
@@ -7,7 +8,9 @@ import { AppErrorBoundary } from './components/AppErrorBoundary.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppErrorBoundary title="LangQuiz failed to load">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppErrorBoundary>
   </StrictMode>,
 )
